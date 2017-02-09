b01bf40bb58d8ae1cd26a68517b2eac91e98cf04
Lucia <lucia.awang416@gmail.com>
2017-02-09 17:51:07 +0000

    waldo exercise using forEach

--- waldo.js
+++ waldo.js
@@ -1,16 +1,24 @@
-// The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
+var list = ["Alice", "Bob", "Waldo", "Winston"];// The second argument/parameter is expected to be a function
+
+function findWaldo(list, found) {
+  list.forEach (function (him, call){
+    if (him === "Waldo"){
+  found(call)
+  }
+ });
+}
+/*
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
       found(i);   // execute callback
     }
   }
 }
-
+*/
 
 function actionWhenFound(x) {
 
   console.log("Found him at " +  x + "!");
 }
 
-findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+findWaldo(list, actionWhenFound);
